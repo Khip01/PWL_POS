dd3127d8474743703ab37bfe398952656bf05265
Khip01[desk] <aakhifathallah24@gmail.com>
2025-03-08 21:49:51 +0700

    feat(layouting): practicum 1
    Layouting AdminLTE
    fix(asset): assets -> asset, test template page

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Testing template view
+Route::get('/template', function () {
+    return view('layouts.template');
+});
