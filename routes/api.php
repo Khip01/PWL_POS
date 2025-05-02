ce6f8e66409c028e35c0a8c79ba95e3c14a56c6b
Khip01[desk] <aakhifathallah24@gmail.com>
2025-05-03 00:51:15 +0700

    feat(level api): practicum 4, js 10
    Implementasi CRUD dalam RESTful API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
@@ -26,3 +27,16 @@ Route::post('/logout', LogoutController::class)->name('logout');
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+// Route::get('levels', [LevelController::class, 'index']);
+// Route::post('levels', [LevelController::class, 'store']);
+// Route::get('levels/{level}', [LevelController::class, 'show']);
+// Route::put('levels/{level}', [LevelController::class, 'update']);
+// Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('levels', [LevelController::class, 'index']);
+    Route::post('levels', [LevelController::class, 'store']);
+    Route::get('levels/{level}', [LevelController::class, 'show']);
+    Route::put('levels/{level}', [LevelController::class, 'update']);
+    Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+});
